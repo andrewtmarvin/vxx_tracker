73dc206ed5f8bd039d5541d97000cb012b52d879
Andrew Marvin <andrewtmarvin@gmail.com>
2021-09-01 17:20:17 -0600

    fixing whitenoise static files issue 6

--- vxx/settings.py
+++ vxx/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
