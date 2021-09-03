3d28f7e176abd36bc04239a4ac23cff605178012
Andrew Marvin <andrewtmarvin@gmail.com>
2021-09-02 20:27:38 -0600

    removed credential file import for production

--- vxx/settings.py
+++ vxx/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os, django_heroku
 import _locale
 _locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
-from credentials import *
+# from credentials import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
